ca32c43849cb5bf699c2d610c23c00067f45a539
quang doan <doan_q@qwasar.io>
2021-06-22 20:58:40 +0000

    added a readme

--- my_printf.c
+++ my_printf.c
@@ -97,19 +97,3 @@ int my_printf(char * restrict format, ...) {
     
 
 }
-
-int main() {
-    char* nullPTR = NULL;
-    char* str = malloc(sizeof(char) * 20);
-    int my_size = 0;
-    int size = 0;
-    my_strcpy(str, "bye");
-    my_size = my_printf("hello ");
-    my_size += my_printf("%c %u %o %d %o %d %x %o %x %s %s %p\n", 65, -30, 8, 8, -8, -8, -15, 'c', 'd', "hello", nullPTR, str);
-
-    size = printf("hello ");
-    size += printf("%c %u %o %d %o %d %x %o %x %s %s %p\n", 65, -30, 8, 8, -8, -8, -15, 'c', 'd', "hello", nullPTR, str);
-    printf("My size: %d\n", my_size);
-    printf("size: %d\n", size);
-    free(str);
-}
